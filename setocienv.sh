c253bcc5efd665e69fa70e9434d9fafbd0a02c10
paj <per.anders.jorgensen@gmail.com>
2018-06-22 16:29:35 +0200

    Added sample terraform environment settings

---
+++ setocienv.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export TF_VAR_tenancy_ocid="<Replace with your oci tenancy OCID>"
+export TF_VAR_user_ocid="<Replace with your oci user OCID>"
+export TF_VAR_fingerprint="<Replace with your pem public key fingerprint>"
+export TF_VAR_private_key_path="<Replace with Path to your pem private key>"
+export TF_VAR_compartment_ocid="<Replace with your oci compartment OCID>"
+export TF_VAR_region="<Replace with your region name, e.g: eu-frankfurt-1>"
+
